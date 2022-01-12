fa391134ef22e46685c3108c244da01c35a375d6
Mariel Hoepelman <marielhoepelman@gmail.com>
2022-01-11 19:46:21 -0500

    Updates Routes.
    - Wraps resources in scope block to route /points (without the prefix 
    /points) to PointsController.
    - Changes the request verb from POST to PATCH in spend route. 
    - Remove the path generator :as from resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/points/add', to: 'points#add', as:'add'
-  post '/points/spend', to: 'points#spend', as:'spend'
-  get '/points/balance', to: 'points#balance', as:'balance'
+  # post '/points/add', to: 'points#add', as:'add'
+  # post '/points/spend', to: 'points#spend', as:'spend'
+  # get '/points/balance', to: 'points#balance', as:'balance'
+
+  scope :points do
+    post '/add', to: 'points#add'
+    patch '/spend', to: 'points#spend'
+    get '/balance', to: 'points#balance'
+  end
 end
